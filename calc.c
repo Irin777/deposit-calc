9421100f87a59f9ec91c0431dbc9ab69e8a863e6
Rin <rin_777@list.ru>
2017-03-15 21:38:10 +0700

    Data inpt

---
+++ calc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+float srok, sum;
+	printf ("¬ведите cумму вклада (в рубл€х) ");
+    scanf ("%f", &sum);
+    printf ("¬ведите срок вклада (в дн€х) ");
+    scanf ("%f", &srok);
+}
