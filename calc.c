e94b48c6769c439791316e03e176a2ba14d71098
Рындина Ирина Анатольевна <ip613s19@mwp10.csc.local>
2017-03-23 09:27:33 +0700

    Corrected

--- calc.c
+++ calc.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-int proverka(float srok,float sum){
+int proverka(float srok,float sum) {
     if ((sum < 10000) || (srok < 0 || srok > 365)) {
-    return 1;
+        return 1;
     }
     else return 0;
 }
 int main() {
-float srok, sum;
-int check;
-	do {
-		printf ("Enter the deposit amount ");
+    float srok, sum;
+    int check;
+    do {
+        printf ("Enter the deposit amount ");
     	scanf ("%f", &sum);
     	printf ("Enter the deposit term ");
     	scanf ("%f", &srok);
-    	check=proverka(srok,sum);
+    	check = proverka(srok,sum);
         if (check == 1) {
             printf ("Invalid input\n");
-        };
-    } while (check);
+        }
+    }
+    while (check);
     if (srok < 31) {
-        sum-=sum/10;
-    };
+        sum -= sum / 10;
+    }
     if ((srok >= 31) && (srok < 121)) {
-      if(sum <= 100000) {
-        sum+=(sum/100)*2;
-      }
-      else (sum+=(sum/100)*3;
-    };
+        if(sum <= 100000) {
+            sum + =(sum / 100) * 2;
+        }
+        else (sum += (sum / 100) * 3;
+    }
     if ((srok >= 121) && (srok < 241)) {
-      if ((sum <= 100000) {
-	  sum+=(sum/100)*6;
+        if ((sum <= 100000) {
+	    sum += (sum / 100) * 6;
 	}
-	else (sum+=(sum/100)*8;
-    };
+	else (sum += (sum / 100) * 8;
+    }
     if (srok >= 241) {
-      if (sum <= 100000){
-        sum+=(sum/100)*12;
-      }
-      else (sum+=(sum/100)*15;
-    };
+        if (sum <= 100000) {
+            sum += (sum / 100) * 12;
+        }
+        else (sum += (sum / 100) * 15;
+    }
     printf("Deposit amount at the end: \n%.0f\n ", sum);
 }
